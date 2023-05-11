cefcf2f9cde85e744151e1c37c15fd17f88bae3d
dingusn10 <87838062+dingusn10@users.noreply.github.com>
2023-05-11 19:06:15 +0000

    added demo planning center api call for kicks and giggles

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
     readonly SPOTIFY_ID: string
     readonly SPOTIFY_KEY: string
+    readonly PCO_APP_ID: string
+    readonly PCO_APP_SECRET: string
 }
 
 interface ImportMeta {
