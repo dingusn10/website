75a080b3b9bdca67541f7f8cb572173cc4cc34d9
dingusn10 <87838062+dingusn10@users.noreply.github.com>
2023-05-16 19:58:39 +0000

    created nav and added state to open mobile nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import { useState } from "react";
+import logo from "../assets/logo.svg";
+import Hamburger from "./Hamburger";
 
 export default function Nav() {
+  const [isVisible, setIsVisible] = useState(false);
+
+ 
+  const handleClick = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
-    <nav id='navBar' className='nav '>
-
-
-    </nav> 
-  )
+    <nav
+      id="navBar"
+      className="mx-auto w-screen flex bg-slate-800 min-h-content"
+    >
+      <div className="container flex flex-row p-4 mx-auto items-center justify-between relative">
+        <a href="" className="">
+          <img src={logo} />
+        </a>
+        <button className='md:hidden' onClick={handleClick}>
+          <Hamburger />
+        </button>
+        {isVisible || window.innerHeight > 767 && (
+          <div
+            className='absolute sm:translate-y-20 bg-slate-500 transition-all rounded-sm md:translate-y-0 md:bg-transparent md:relative md:p-2'>
+            <ul className="py-4 px-6 text-white flex flex-col md:flex-row gap-4 md:justify-around md:gap-8 md:items-center">
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Book Us</li>
+              <li>Schedule</li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 }
