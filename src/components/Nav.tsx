2d4f25e598c0a015eb4fcd8381ba44f270cbe54d
dingusn10 <87838062+dingusn10@users.noreply.github.com>
2023-05-16 20:09:03 +0000

    fixed logic for nav view between mobile and desktop... i hope

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import logo from "../assets/logo.svg";
 import Hamburger from "./Hamburger";
 
 export default function Nav() {
   const [isVisible, setIsVisible] = useState(false);
 
- 
+  const desktop = window.innerWidth > 767;
+
+  useEffect(() => {
+    if (desktop){
+      return setIsVisible(true)
+    }
+  })
+
   const handleClick = () => {
     setIsVisible(!isVisible);
   };
@@ -22,7 +29,7 @@ export default function Nav() {
         <button className='md:hidden' onClick={handleClick}>
           <Hamburger />
         </button>
-        {isVisible || window.innerHeight > 767 && (
+        {isVisible && (
           <div
             className='absolute sm:translate-y-20 bg-slate-500 transition-all rounded-sm md:translate-y-0 md:bg-transparent md:relative md:p-2'>
             <ul className="py-4 px-6 text-white flex flex-col md:flex-row gap-4 md:justify-around md:gap-8 md:items-center">
